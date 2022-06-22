e1303463642f3862837124c6bc67255a81197103
naivvve <1532619822@qq.com>
2022-06-22 09:50:48 +0800

    6.20

--- work_1/sort_3.cpp
+++ work_1/sort_3.cpp
@@ -6,13 +6,17 @@
 #include <string.h>
 using namespace std; //随机数函数头文件
 int count1=0,count2=0,count3=0;
+double l1, m1,l2, m2,l3, m3,end1=0,end2=0,end3=0;
 void BubbleSort(int num[], int n)//冒泡排序
 {
     for (int i=0; i<n-1; i++)//两个for循环实现排序
         for (int j=0; j<n-i-1; j++){
             count1++;
-            if (num[j]>num[j+1])  //当前元素较大时
+            l1=(double)clock();
+            if (num[j]>num[j+1])//当前元素较大时
             {
+                m1=(double)clock();
+                end1+=(m1-l1);
                 int temp=num[j];//exchange
                 num[j]=num[j+1];
                 num[j+1]=temp;
@@ -23,8 +27,11 @@ void BubbleSort(int num[], int n)//冒泡排序
 void merge(int num[],int left[],int right[],int l,int r)//两个有序序列的合并
 {
     int i=0,j=0,p=0;
+    l2=(double)clock();
     while(i<l&&j<r)//较小的放入num
     {
+        m2=(double)clock();
+        end2+=(m2-l2);
         count2++;
         if(left[i]<=right[j])
             num[p++]=left[i++];
@@ -67,13 +74,19 @@ void quickSort(int a[], int low,int high)//快速排序
         int target=a[low];//待排序数组的第一个数为目标数target
         while(i<j)
         {
+            l3=(double)clock();
             while(i<j&&a[j]>=target){
+                m3=(double)clock();
+                end3+=(m3-l3);
                 j--;//右侧
                 count3++;
             }
             if(i<j)
                 a[i++]=a[j];
+            l3=(double)clock();
             while(i<j&&a[i]<=target){
+                m3=(double)clock();
+                end3+=(m3-l3);
                 i++;//左侧
                 count3++;
             }
@@ -98,7 +111,7 @@ void print(int arr[], int n)//输出函数
 int main()
 {
     int n,a[100000],b[10000],c[10000];
-    double l1, m1,l2, m2,l3, m3;
+
     cout<<"请输入要生成随机数个数的参数n：";
     cin>>n;
     srand(time(0));
@@ -110,16 +123,13 @@ int main()
     cout<<"*********************************************************************************************************************** \n";
     memcpy(b,a,sizeof(b));
     memcpy(c,a,sizeof(b));
-    l1=(double)clock();
     BubbleSort(a,n);//冒泡排序函数
     cout<<"冒泡排序后数组如下：\n";
     print(a,n);
     cout<<"冒泡排序基本操作的次数为：\n";
     printf("%d\n", count1);
     cout<<"冒泡排序耗费的时间为：\n";
-    m1=(double)clock();
-    printf("%.4fms\n", (m1-l1));
-    l2=(double)clock();
+    printf("%.4fms\n", end1);
     mergeSort(b,n);//合并排序函数
     cout<<"*********************************************************************************************************************** \n";
     cout<<"合并排序后生成的数组为：\n";
@@ -127,9 +137,7 @@ int main()
     cout<<"合并排序基本操作的次数为：\n";
     printf("%d\n", count2);
     cout<<"合并排序耗费的时间为：\n";
-    m2=(double)clock();
-    printf("%.4fms\n", (m2-l2));
-    l3=(double)clock();
+    printf("%.4fms\n", end2);
     quickSort(c,0,n-1);//快速排序函数
     cout<<"*********************************************************************************************************************** \n";
     cout<<"快速排序后生成的数组为：\n";
@@ -137,7 +145,6 @@ int main()
     cout<<"快速排序基本操作的次数为：\n";
     printf("%d\n", count3);
     cout<<"快速排序耗费的时间为：\n";
-    m3=(double)clock();
-    printf("%.4fms\n", (m3-l3));
+    printf("%.4fms\n", end3);
     return 0;
 }
